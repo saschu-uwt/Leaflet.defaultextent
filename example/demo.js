6b9227829183fc9a7dcff84f2a5f4cd9215fb35a
saschu-uwt <66031137+saschu-uwt@users.noreply.github.com>
2021-06-07 13:29:21 -0700

    Update demo.js

--- example/demo.js
+++ example/demo.js
@@ -1,18 +1,18 @@
 /* global L */
 
-var mapCenter = [29.76, -95.38];
+
 
 // Set up map
 var map = L.map('map', {
-  defaultExtentControl: true,
-  center: mapCenter,
-  zoom: 14
+  // defaultExtentControl: true,
+  center: [47.24, -122.45],
+  zoom: 10
 });
 
 // Another way to add to map
-// L.control.defaultExtent().addTo(map);
+L.control.defaultExtent().addTo(map);
 
-L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
+L.tileLayer('https://api.mapbox.com/styles/v1/saschu/ckp4g6sjf1jwt18r0ffjwxamf/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic2FzY2h1IiwiYSI6ImNrZ3poNGVkYjA1b3Ayd3JzOHczb29iNjEifQ.MqXTIcUhZl4C-s0Jk5o49A', {
   maxZoom: 18,
   id: 'examples.map-i86knfo3'
 }).addTo(map);
